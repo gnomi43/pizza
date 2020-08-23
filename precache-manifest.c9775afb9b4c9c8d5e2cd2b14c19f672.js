e6dedda07a330b3745912c7f69b7553940c27a07
Nikita Baranov <khabnet47@mail.ru>
2020-08-23 20:13:04 +0300

    Updates

---
+++ precache-manifest.c9775afb9b4c9c8d5e2cd2b14c19f672.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1b130568cfe38b1d3a6eea2d49b1bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1377f526fd9ca5403bc",
+    "url": "/static/css/main.385ee760.chunk.css"
+  },
+  {
+    "revision": "6e0d06f3b12af77a1bee",
+    "url": "/static/js/2.742d6529.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.742d6529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1377f526fd9ca5403bc",
+    "url": "/static/js/main.d14d37aa.chunk.js"
+  },
+  {
+    "revision": "a02b8dcb335374831747",
+    "url": "/static/js/runtime-main.c32e935e.js"
+  },
+  {
+    "revision": "609606ec0867d5d4a885629aa1f41344",
+    "url": "/static/media/logo.609606ec.png"
+  },
+  {
+    "revision": "66755e298317d9024e10a2b5b0113b9b",
+    "url": "/static/media/pizza_1.66755e29.webp"
+  },
+  {
+    "revision": "4fedd2af4ff2ae57a190da7861c0cf7f",
+    "url": "/static/media/pizza_2.4fedd2af.webp"
+  },
+  {
+    "revision": "fec6f0795fe596dd332ddfa74fb8b938",
+    "url": "/static/media/pizza_3.fec6f079.webp"
+  },
+  {
+    "revision": "5b61755013f1d006dec48a1e4eee3b1e",
+    "url": "/static/media/pizza_4.5b617550.webp"
+  },
+  {
+    "revision": "68d1a366a02e97325c416e199de96edc",
+    "url": "/static/media/pizza_5.68d1a366.webp"
+  },
+  {
+    "revision": "47db411d4372520b6c418afe2eba6ed6",
+    "url": "/static/media/pizza_6.47db411d.webp"
+  },
+  {
+    "revision": "513ace6e1b370bedf80c20b569ccdbc1",
+    "url": "/static/media/pizza_7.513ace6e.webp"
+  },
+  {
+    "revision": "d409384726edf0546d0fda84ad69d5f0",
+    "url": "/static/media/pizza_8.d4093847.webp"
+  },
+  {
+    "revision": "d9ea8f1e8309955ba56c4edce60da424",
+    "url": "/static/media/pizza_9.d9ea8f1e.webp"
+  }
+]);
